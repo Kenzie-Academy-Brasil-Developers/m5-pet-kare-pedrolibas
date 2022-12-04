a757625be17f616323fdb552d8daf36269423698
pedrolibas <pedrogv03@hotmail.com>
2022-12-04 02:10:04 -0300

    feat: create table DER and models groups, pets and traits

---
+++ pets/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+class SexPet(models.TextChoices):
+    MALE = "Male"
+    FEMALE = "Famale"
+    NOT_INFORMED = "Not Informed"
+
+
+class Pet(models.Model):
+    name = models.CharField(max_length=50)
+    age = models.IntegerField()
+    weight = models.FloatField()
+    sex = models.CharField(max_length=20, choices=SexPet.choices, default=SexPet.NOT_INFORMED)
+    group = models.ForeignKey("groups.Group", on_delete=models.CASCADE, related_name="pets", null=True)
