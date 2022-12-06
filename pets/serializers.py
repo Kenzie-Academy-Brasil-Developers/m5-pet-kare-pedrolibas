2e791d12ec98e944bea1efce1fe1dcdc25753c4b
pedrolibas <pedrogv03@hotmail.com>
2022-12-05 21:26:18 -0300

    feat: create in serializers

--- pets/serializers.py
+++ pets/serializers.py
@@ -1,11 +1,9 @@
 from rest_framework import serializers
-
-class GroupSerializer(serializers.Serializer):
-    scientific_name = serializers.CharField()
-
-
-class TraitSerializer(serializers.Serializer):
-    name = serializers.CharField()
+from groups.serializers import GroupSerializer
+from traits.serializers import TraitSerializer
+from pets.models import SexPet, Pet
+from groups.models import Group
+from traits.models import Trait
 
 
 class PetSerializer(serializers.Serializer):
@@ -13,6 +11,43 @@ class PetSerializer(serializers.Serializer):
     name = serializers.CharField()
     age = serializers.IntegerField()
     weight = serializers.FloatField()
-    sex = serializers.CharField()
+    sex = serializers.ChoiceField(choices=SexPet.choices, default=SexPet.NOT_INFORMED)
     group = GroupSerializer()
     traits = TraitSerializer(many=True)
+
+
+    def create(self, validated_data: dict):
+        group_dict = validated_data.pop("group")
+        traits = validated_data.pop("traits")
+        
+        pet = Pet.objects.create(**validated_data)
+
+        group = Group.objects.get_or_create(scientific_name=group_dict["scientific_name"])
+        pet.group = group[0]
+
+        for trait in traits:
+            trait_data = Trait.objects.get_or_create(name=trait["name"])
+            pet.traits.add(trait_data[0])
+
+        return pet
+
+
+    def update(self, instance, validated_data: dict):
+        group_dict = validated_data.pop("group")
+        traits = validated_data.pop("traits")
+
+        for key, value in validated_data.items():
+            setattr(instance, key, value)
+
+        # instance.name = validated_data.get("name", instance.name)
+        # instance.age = validated_data.get("age", instance.age)
+        # instance.weight = validated_data.get("weight", instance.weight)
+        # instance.sex = validated_data.get("sex", instance.sex)
+        # instance.group = Group.objects.get_or_create(scientific_name=validated_data["group"]["scientific_name"])[0]
+        # instance.traits.set = [Trait.objects.get_or_create(name=trait["name"])[0] for trait in validated_data["traits"]]
+
+        print("teste serializer")
+
+        instance.save()
+
+        return instance
--- pets/views.py
+++ pets/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from rest_framework.views import APIView, Response, Request, status
+from .models import Pet
+from .serializers import PetSerializer
+from django.shortcuts import get_object_or_404
 
-# Create your views here.
+
+class PetView(APIView):
+    def post(self, request: Request) -> Response:
+        serializer = PetSerializer(data=request.data)
+        
+        serializer.is_valid(raise_exception=True)
+
+        serializer.save()
+
+        return Response(serializer.data, status.HTTP_201_CREATED)
+
+
+class PetDetailView(APIView):
+    def patch(self, request: Request, pet_id: int) -> Response:
+        pet = get_object_or_404(Pet, id=pet_id)
+        print("teste patch")
+        serializer = PetSerializer(pet, data=request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+
+        return Response(serializer.data)
+
